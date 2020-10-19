c02cfbcbede055a5f84e24d16936de7c018dd1b4
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-10-19 15:53:41 +0900

    Learned Map

--- src/index.js
+++ src/index.js
@@ -3,3 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('tomato'));
+//(중요)React Application은 한 번에 하나의 component만 rendering 가능
+//그러므로 모든 것은 ./App에 들어가야한다.
