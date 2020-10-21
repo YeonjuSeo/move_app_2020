8bd25af0a0d32b212441bbdd6021b4fe0c9e9f75
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-10-21 21:38:57 +0900

    axios & reading API

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
 import React from "react";
 //같은 디렉토리 안에 있을 때는 ./으로 위치가 표시됨
-import PropTypes from "prop-types";
+import axios from "axios";
+//data fetch를 위한 axios
 
 class App extends React.Component {
   state = {
-    //변하는 데이터를 위한 object인 state
-    count: 0,
+    isLoading: true,
   };
 
-  add = () => {
-    console.log("add");
-    this.setState((current) => ({ count: current.count + 1 }));
-    // this.setState({ count: this.state.count + 1 })를 대체하는 더 좋은 코드
+  getMovies = async () => {
+    //처리되는데 시간이 좀 걸리는 axios를 기다린다는 의미의 async와 await
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
+    console.log(movies);
   };
-  minus = () => {
-    console.log("minus");
-    this.setState((current) => ({ count: current.count - 1 }));
-  };
+
   componentDidMount() {
-    console.log("component rendered");
+    this.getMovies();
   }
-  componentDidUpdate() {
-    console.log("I just updated");
-  }
+
   render() {
-    console.log("I'm rendering.");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
   }
 }
 
