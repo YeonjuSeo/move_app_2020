a0ca7729db919c73fa8d5e29e97776ebcdd71b24
YeonjuSeo <56028436+YeonjuSeo@users.noreply.github.com>
2020-10-21 22:22:49 +0900

    Reading & Mapping Data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-//같은 디렉토리 안에 있을 때는 ./으로 위치가 표시됨
 import axios from "axios";
 //data fetch를 위한 axios
+import Movie from "./movies";
+//같은 디렉토리 안에 있을 때는 ./으로 위치가 표시됨
 
 class App extends React.Component {
   state = {
     isLoading: true,
+    movies: [],
   };
 
   getMovies = async () => {
@@ -14,8 +16,11 @@ class App extends React.Component {
       data: {
         data: { movies },
       },
-    } = await axios.get("https://yts-proxy.now.sh/list_movies.json");
-    console.log(movies);
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({ movies, isLoading: false });
+    //==this.setSate({movies:movies}); 왼쪽은 state의 movies 오른쪽은 axios에서 온 movie
   };
 
   componentDidMount() {
@@ -23,8 +28,23 @@ class App extends React.Component {
   }
 
   render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? "Loading" : "We are ready"}</div>;
+    const { isLoading, movies } = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map((movie) => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('tomato'));
+ReactDOM.render(<App />, document.getElementById("tomato"));
 //(중요)React Application은 한 번에 하나의 component만 rendering 가능
 //그러므로 모든 것은 ./App에 들어가야한다.
